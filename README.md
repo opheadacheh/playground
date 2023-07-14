eec37f9a3c39ae63a50ab89579323da0371c42b8
Wanjia He <wanjia1.he@momenta.ai>
2023-07-15 01:27:17 +0800

    add base images and init go server

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # playground
 
 This is Wanjia's coding playground
+
+# build and push docker image
+export TAG=ghcr.io/opheadacheh/bazel_build_base:0.3
+docker build -f BuildBaseDockerImages/v0.3/Dockerfile . -t $TAG && docker push $TAG
+
+# enter build base
+docker run -v $(pwd):/playground -it --rm ghcr.io/opheadacheh/bazel_build_base:0.3
---
+++ ServerTemplate/server/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	helloworldpb "project/proto"
+
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func (s *server) SayHello(ctx context.Context, in *helloworldpb.HelloRequest) (*helloworldpb.HelloReply, error) {
+	return &helloworldpb.HelloReply{Message: "Hello, " + in.Name}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	helloworldpb.RegisterGreeterServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
